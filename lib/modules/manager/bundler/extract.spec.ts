399ba5bdbbbf4fb0873633442b3f332f5547b324
Julien Tanay <julien.tanay@doctolib.com>
2024-11-30 16:03:59 +0100

    feat(bundler): gracefully ignore internal packages

--- lib/modules/manager/bundler/extract.spec.ts
+++ lib/modules/manager/bundler/extract.spec.ts
@@ -250,4 +250,21 @@ describe('modules/manager/bundler/extract', () => {
       ],
     });
   });
+
+  it('skips local gems in Gemfile', async () => {
+    const pathGemfile = codeBlock`
+      gem 'foo', path: 'vendor/foo'
+      `;
+
+    fs.readLocalFile.mockResolvedValueOnce(pathGemfile);
+    const res = await extractPackageFile(pathGemfile, 'Gemfile');
+    expect(res).toMatchObject({
+      deps: [
+        {
+          depName: 'foo',
+          skipReason: 'internal-package',
+        },
+      ],
+    });
+  });
 });
